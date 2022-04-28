c45acf2abccbd83e3e020d69472fbfce91d31cb1
luisKisters <luis.w.kisters@gmail.com>
2022-04-28 22:36:22 +0200

    removed some not needed imports

--- data/nltk_utils.py
+++ data/nltk_utils.py
@@ -1,16 +1,16 @@
-import nltk
-from nltk.stem.porter import PorterStemmer
 import numpy as np
-import torch
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
+import nltk
+# nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 
+
 def stem(word):
     return stemmer.stem(word.lower())
+
 
 def bag_of_words(tokenized_sentence, all_words):
     tokenized_sentence = [stem(w) for w in tokenized_sentence]
