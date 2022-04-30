c9c1238fc337d944aebaa25d4ddd872b3816ebe6
luisKisters <luis.w.kisters@gmail.com>
2022-04-30 05:26:22 +0200

    just ignore that commit ok?

---
+++ data/test.py
@@ -0,0 +1,9 @@
+patterns = "test"
+responses = "test"  
+# tag_intent = ""
+tag_intent = f"
+    "tag_intent": tag_intent,
+    "patterns": patterns,
+    "responses": responses
+"
+print(tag_intent)
